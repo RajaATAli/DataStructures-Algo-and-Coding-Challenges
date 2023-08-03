dc61e9c25355c65695215e0eabf0ecf9c394bd20
RajaATAli <allmdararslan13@gmail.com>
2023-08-02 23:37:55 -0600

    i/o DSA Example added

---
+++ DSAC++/io.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int amount1;
+  cout<<"Please enter amount 1 = ";
+  cin >> amount1;
+
+  int amount2;
+  cout<<"Please enter amount 2 = ";
+  cin >> amount2;
+
+  int sum = amount1 + amount2;
+  cout<<"This is the total amount = "<<sum<<endl; //endl means endline for going into next line
+
+
+  return 0;
+}
