ebcc571602d5154df4943bb451630c92e6dbdc37
RajaATAli <allmdararslan13@gmail.com>
2023-08-23 23:55:34 -0400

    Pattern design updated

--- DSAC++/halfpyramid180deg.cpp
+++ DSAC++/halfpyramid180deg.cpp
@@ -1,1 +1,12 @@
 // Half Pyramid with 180 deg rotation
+// We will have to add some sort of space as they are being printed towards the left
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  
+
+
+  return 0;
+}
