ed3212d7305343b89817f54906af2a210edc41ec
RajaATAli <allmdararslan13@gmail.com>
2023-08-31 23:30:49 -0400

    Daily commit for practice

--- DSAC++/naturalnums.cpp
+++ DSAC++/naturalnums.cpp
@@ -2,8 +2,19 @@
 #include<iostream>
 using namespace std;
 
+int sum(int n){
+  int ans;
+  ans = 0;
+
+  for (int i = 1; i <= n; i++){
+    ans += i;
+  }
+  return ans;
+}
+
 int main(){
-
-
+  int num;
+  cin >> num;
+  cout << sum(num) << endl;
   return 0;
 }
---
+++ DSAC++/pythogoreantriplet.cpp
@@ -0,0 +1,21 @@
+//Check whether a triplet is a pythogorean triplet or not
+#include<iostream>
+using namespace std;
+
+bool triplet(int x, int y, int z){
+  
+
+
+}
+
+int main(){
+  int x,y,z;
+  cin >> x >> y >> z;
+  if (triplet(x,y,z)){
+    cout << "It is a triplet";
+  }
+  else{
+    cout << "Not a triplet";
+  }
+  return 0;
+}
