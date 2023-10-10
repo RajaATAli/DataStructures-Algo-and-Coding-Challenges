1bc4174e9ed75c5c4614801f31fbe9ca8990b39b
RajaATAli <allmdararslan13@gmail.com>
2023-10-09 23:58:16 -0400

    Changes made to pythogorean triplet

--- DSAC++/pythogoreantriplet.cpp
+++ DSAC++/pythogoreantriplet.cpp
@@ -1,8 +1,13 @@
 //Check whether a triplet is a pythogorean triplet or not
 #include<iostream>
+//#include<math.h>
+#include<algorithm>
 using namespace std;
-
+// a^2 = b^2 + c^2
+// a here is the maximum
 bool triplet(int x, int y, int z){
+  // finding maximum of the triplets and assigning it to a
+  int a = max(x, max(y,z));
   
 
 
