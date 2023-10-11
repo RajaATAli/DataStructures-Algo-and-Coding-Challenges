4eacc748c9d86c6e8eb0dfd380f930f57be50a68
RajaATAli <allmdararslan13@gmail.com>
2023-10-10 23:57:08 -0400

    pythogorean triplet example added

--- DSAC++/pythogoreantriplet.cpp
+++ DSAC++/pythogoreantriplet.cpp
@@ -8,9 +8,25 @@ using namespace std;
 bool triplet(int x, int y, int z){
   // finding maximum of the triplets and assigning it to a
   int a = max(x, max(y,z));
-  
-
-
+  int b,c;
+  if (a == x){
+    b = y;
+    c = z;
+  }
+  else if (a == y){
+    b = x;
+    c = z;
+  }
+  else{
+    b = x;
+    c = y;
+  }
+  if (a*a == b*b + c*c){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 int main(){
