b2bc7d15a28b742efb08fee5c83021aebac79b6a
RajaATAli <allmdararslan13@gmail.com>
2023-11-02 18:35:21 -0400

    Added deletion functionality in linked list

--- DSAC++/Linked List/linkedlist.cpp
+++ DSAC++/Linked List/linkedlist.cpp
@@ -7,6 +7,7 @@ class node{
   int data;
   node* next; // Pointer to next node
 
+  // Constructor
   node(int val){
     data = val;
     next = NULL;
@@ -22,6 +23,7 @@ void insertAtTail(node* &head, int val){
   }
 
   node* temp = head;
+  // Traversing the list
   while(temp->next!=NULL){
     temp=temp->next;
   }
@@ -47,6 +49,37 @@ void insertAfterNode(node* &head, int valueAfter, int val) {
     } else {
         cout << "Node with value " << valueAfter << " not found." << endl;
     }
+}
+
+// Delete a specific node Function
+void Deletion(node* &head, int key){
+  // Case 1: There is no node to be deleted
+  if (head == nullptr){
+    return;
+  }
+
+  // Case 2: Deleting the Head Node
+  if (head->data == key){
+      node* toDelete = head; // This node will be later deleted
+      head = head->next; // The head now points to the next node
+      delete toDelete;
+      return;
+    }
+
+  // Case 3: Traversing the List and Deleting a Node (specifically non-head)
+
+  node* prev = nullptr;
+  node* temp = head;
+  while(temp != nullptr && temp->data != key){
+    prev = temp; // Keep track of node before temp
+    temp = temp->next; // Move to the next node
+  }
+  if (temp != nullptr){
+    // relinking nodes before the deleted node and after the deleted node
+    // Skip over the node to delete
+    prev -> next = temp -> next;
+    delete temp;
+  }
 }
 
 bool search(node* head, int key){
@@ -85,9 +118,14 @@ int main (){
   insertAfterNode(head, 2, 5); // Insert 5 after 2
   display(head);
 
-  cout<<search(head,10)<<endl;
-  cout<<search(head,2)<<endl;
-  
+  cout<<search(head,10)<<endl; // 0
+  cout<<search(head,2)<<endl; // 1
+
+  Deletion(head,2);
+  cout<< "List after deleting 2:" <<endl;
+  display(head);
+
+  cout<<search(head,2)<<endl; // 0
 
   return 0;
 }
