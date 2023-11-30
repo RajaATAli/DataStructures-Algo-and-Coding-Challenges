37c68c89c468561c3c6003e365bb24fe59704154
RajaATAli <allmdararslan13@gmail.com>
2023-11-29 23:50:49 -0500

    FileSystem problem added

---
+++ Misc Problems/FileSystem.cpp
@@ -0,0 +1,32 @@
+/*Problem: Design a File System
+
+You are tasked to design a basic file system. This file system should allow the following operations:
+
+Create a file: Given a string path (like "/a/b/c") and an integer value, create a new file at the specified path. If any directories in the path do not exist, they should be created. Each file stores a single integer value.
+
+Get the value of a file: Given a string path to a file, return the value stored in that file. If the file does not exist, return -1.
+
+Consider the following points:
+
+The root directory is represented as "/".
+The file system should support nested paths (directories within directories).
+You can assume that the file paths will always be valid and will include only letters, numbers, and slashes.
+You need to optimize for efficient file creation and retrieval. */
+
+#include <iostream>
+
+class FileSystem{
+
+
+
+
+  void createFile(){
+
+  }
+
+  int valueFile(){
+
+    return -1;
+  }
+};
+
